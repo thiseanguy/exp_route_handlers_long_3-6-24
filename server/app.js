8aedc2758ebd1dd1e77df351da0a58cf4b432c4d
Sean Hatcher <thiseanguy@gmail.com>
2024-03-07 22:18:17 -0600

    test 9 passed

--- server/app.js
+++ server/app.js
@@ -36,9 +36,34 @@ app.get("/artists", (req, res, next) => {
 app.post("/artists", (req, res, next) => {
   // req.body({name});
   res.status(201);
-
   res.send(addArtist(req.body))
   next()
+})
+
+app.get("/artists/:artistId", (req, res, next) => {
+  res.status(200);
+  console.log(getArtistByArtistId(req.params.artistId));
+  res.json(getArtistByArtistId(req.params.artistId))
+})
+
+app.delete("/artists/:artistId", (req, res, next) => {
+  res.status(200);
+  console.log()
+  deleteArtistByArtistId(req.params.artistId)
+  res.json({
+    "message": "Successfully deleted"
+  })
+  next()
+})
+
+app.get("/artists/:artistId/albums", (req, res, next) =>{
+  res.status(200);
+  res.json(getAlbumsByArtistId(req.params.artistId))
+})
+
+app.get("/albums/:albumId", (req, res, next) =>{
+  res.status(200);
+  res.json(getAlbumByAlbumId(req.params.albumId))
 })
 
 
